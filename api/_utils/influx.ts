da123db45fde88cc6cdecec415405e1e6f74645a
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2021-11-18 01:16:07 +0900

    feat: update influx db last price of bitso

---
+++ api/_utils/influx.ts
@@ -0,0 +1,35 @@
+import {
+  InfluxDB,
+  Point,
+} from "https://cdn.skypack.dev/@influxdata/influxdb-client-browser?dts";
+
+const token = Deno.env.get("INFLUX_DB_TOKEN")!;
+const org = Deno.env.get("INFLUX_DB_ORG")!;
+const bucket = Deno.env.get("INFLUX_DB_BUCKET")!;
+
+const client = new InfluxDB({
+  url: "https://ap-southeast-2-1.aws.cloud2.influxdata.com",
+  token,
+});
+
+const WriteApi = client.getWriteApi(org, bucket);
+
+type LastPrice = {
+  label: string;
+  price: number;
+};
+
+function getPoints(market: string, last: LastPrice[]): ArrayLike<Point> {
+  return last.map(({ label, price }) => {
+    return new Point(market).floatField(label, price);
+  });
+}
+
+function writeBatch(market: string, last: LastPrice[]) {
+  const points = getPoints(market, last);
+
+  WriteApi.writePoints(points);
+  return WriteApi.flush();
+}
+
+export { writeBatch };
