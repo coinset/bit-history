728fde924d983f060f0e5bd2bf743370099c49a6
TomokiMiyauci <development.operation.6.6.6@gmail.com>
2022-01-07 11:20:31 +0900

    feat(api): add collecting btcbox last price

---
+++ api/btcbox/last_price.ts
@@ -0,0 +1,29 @@
+import { writeBatch } from "../_utils/influx.ts";
+import {
+  fetchTickers,
+  TickerResponse,
+} from "https://deno.land/x/btcbox@v1.0.0-beta.2/mod.ts";
+import type { APIGatewayProxyResultV2 } from "../deps.ts";
+import type { LastPrice } from "../_utils/influx.ts";
+
+export async function getLastPrices(): Promise<LastPrice[]> {
+  return Object.entries(await fetchTickers()).map(([key, value]) => ({
+    label: key.replace("_", ""),
+    price: (value as TickerResponse).last,
+  }));
+}
+
+export async function handler(): Promise<APIGatewayProxyResultV2> {
+  try {
+    const lastPrices = await getLastPrices();
+    writeBatch("btcbox", lastPrices);
+    return {
+      statusCode: 200,
+    };
+  } catch (e) {
+    console.error(e);
+    return {
+      statusCode: 500,
+    };
+  }
+}
--- api/dev_deps.ts
+++ api/dev_deps.ts
@@ -3,14 +3,15 @@ import {
   jestExtendedMatcherMap,
   jestMatcherMap,
   jestModifierMap,
-} from "https://deno.land/x/unitest@v1.0.0-beta.52/mod.ts";
-export { defineGlobalThis } from "https://deno.land/x/unitest@v1.0.0-beta.52/mock/global_this.ts";
+} from "https://deno.land/x/unitest@v1.0.0-beta.79/mod.ts";
+export { defineGlobalThis } from "https://deno.land/x/unitest@v1.0.0-beta.79/mock/global_this.ts";
 export {
   anyArray,
   anyNumber,
+  anyOf,
   stringMatching,
   test,
-} from "https://deno.land/x/unitest@v1.0.0-beta.52/mod.ts";
+} from "https://deno.land/x/unitest@v1.0.0-beta.79/mod.ts";
 
 const expect = defineExpect({
   matcherMap: {
